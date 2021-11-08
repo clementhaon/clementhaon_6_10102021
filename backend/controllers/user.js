0c620d16b80d5aced6de114fe3b7f9df80067c83
clementhaon <clement.haon.13@gmail.com>
2021-11-08 21:17:44 +0100

    last commit

---
+++ backend/controllers/user.js
@@ -0,0 +1,57 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cryptojs = require('crypto-js');
+
+const User = require('../models/user');
+require('dotenv').config();
+
+exports.signup = (req, res, next) => {
+
+  const regexEmail = /^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i;
+
+  if (regexEmail.test(req.body.email)){
+      
+      bcrypt.hash(req.body.password, 10)
+
+      .then(hash => {
+          const user = new User({
+              email: cryptojs.HmacSHA256(req.body.email, process.env.EMAIL_KEY).toString(),
+              password: hash
+          });
+          user.save()
+              .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
+              .catch( error => res.status(400).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+  }
+
+  else {
+      res.status(400).json({ message : 'Email invalide'})
+  }
+};
+
+  exports.login = (req, res, next) => {
+    const cryptedEmail = cryptojs.HmacSHA256(req.body.email, process.env.EMAIL_KEY).toString();
+    User.findOne( { email: cryptedEmail })
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        }
+        bcrypt.compare(req.body.password, user.password)
+          .then(valid => {
+            if (!valid) {
+              return res.status(401).json({ error: 'Mot de passe incorrect !' });
+            }
+            res.status(200).json({
+              userId: user._id,
+              token: jwt.sign(
+                { userId: user._id },
+                process.env.TOKEN_KEY,
+                { expiresIn: '24h' }
+              )
+            });
+          })
+          .catch(error => res.status(500).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+  };
